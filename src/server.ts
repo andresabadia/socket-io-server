a338e263639072e63ce6ec372fc149fd40f8ab5c
Andres Abadia <a.abadia@bitpioneers.com>
2021-06-12 12:25:50 +0200

    managing connections on the server

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,8 @@ const io = new Server(server, {
     cors: { origin: "*", methods: ["GET", "POST"] },
 });
 const PORT: number = 8080;
+let connections: { id: string; isReady: boolean }[] = [];
+let controller: string = "";
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,10 +27,33 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => res.send("0.2"));
 
 io.on("connection", (socket) => {
-    console.log("a user connected");
-    socket.emit("connected", true);
+    console.log("a user connected", socket.handshake.auth.token);
+    if (socket.handshake.auth.token) {
+        controller = socket.id;
+        socket.emit("connected", connections);
+    } else {
+        connections.push({ id: socket.id, isReady: false });
+        socket.emit("connected", true);
+        if (controller !== "") {
+            socket.to(controller).emit("connected", connections);
+        }
+    }
     socket.on("disconnect", () => {
-        console.log("a user disconnected");
+        console.log("a user disconnected", socket.id);
+        console.log(connections);
+        if (socket.id === controller) {
+            controller = "";
+        } else {
+            connections = [
+                ...connections.filter(
+                    (connection) => socket.id !== connection.id
+                ),
+            ];
+        }
+        if (controller !== "") {
+            socket.to(controller).emit("connected", connections);
+        }
+        console.log(connections);
     });
     socket.on("update-pano-view", (panoView) => {
         socket.volatile.broadcast.emit("pano-view-update", panoView);
@@ -38,6 +63,18 @@ io.on("connection", (socket) => {
     });
     socket.on("el-clicked", (el) => {
         socket.broadcast.emit("el-clicked", el);
+    });
+    socket.on("user-ready", (_isReady) => {
+        console.log("user-ready");
+        if (controller !== "") {
+            const connnectionIndex = connections.findIndex(
+                (connection) => connection.id === socket.id
+            );
+            if (connnectionIndex >= 0) {
+                connections[connnectionIndex].isReady = true;
+            }
+            socket.to(controller).emit("connected", connections);
+        }
     });
 });
 
